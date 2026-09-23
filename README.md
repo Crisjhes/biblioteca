b314797e7d58991db309e1ad4ffb9215e7b579cb
Cristhian Rios <riosparedescristhian@gmail.com>
2026-09-22 21:38:43 -0400

    Implementa formulario de inicio de sesión

---
+++ README.md
@@ -0,0 +1,6 @@
+# Portal de Biblioteca
+
+Aplicación web desarrollada con Python y Flask.
+
+Materia:
+TEM-742 Tecnologías Emergentes II
--- app.py
+++ app.py
@@ -1,7 +1,46 @@
-from flask import Flask, render_template, request, redirect, url_for, session, make_response
+from flask import Flask, render_template, request, redirect, url_for, session
 
 
 app = Flask(__name__)
+
+app.secret_key = "clave_secreta_biblioteca"
+
+usuarios = {
+
+    "carlos": "1111",
+    "laura": "2222",
+    "diego": "3333"
+
+}
+@app.route("/")
+def inicio():
+    return render_template("base.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    mensaje = ""
+    if request.method == "POST":
+        usuario = request.form.get("usuario")
+        contraseña = request.form.get("contraseña")
+        if usuario in usuarios and usuarios[usuario] == contraseña:
+            session["usuario"] = usuario
+
+            return redirect(
+                url_for("inicio")
+            )
+
+
+        else:
+
+            mensaje = "Usuario o contraseña incorrectos."
+
+
+    return render_template(
+        "login.html",
+        mensaje=mensaje
+    )
+
+
 
 
 
